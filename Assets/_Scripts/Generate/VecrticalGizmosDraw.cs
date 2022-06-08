1fd5183f4d424a714f6fe73e1bfb0aa483030d57
Vitya <vitiakyselov1@gmail.com>
2022-06-08 20:43:03 +0300

    small changes

---
+++ Assets/_Scripts/Generate/VecrticalGizmosDraw.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VecrticalGizmosDraw : MonoBehaviour
+{
+    private Vector3 areaCube = new Vector3(4, 8, 0); // horizontal cube
+    // 16 x 10 +-
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(transform.position, areaCube);
+    }
+}
